a8dec60075af6a10233d95a28202a60c7cb481f9
ONE100 <baegi9@naver.com>
2024-09-28 11:44:31 +0900

    nestjs 대시보드

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { pretendard } from './ui/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={pretendard.className}>{children}</body>
+    </html>
+  );
+}
